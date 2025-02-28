e550b9c5a6b88ddfe421d9848abfa4e1274a1356
Subu <Subramanyammolleti@gmail.com>
2025-02-27 22:05:46 -0500

    React with Parcel and React packages

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Learn React
+## Learn React 🚀
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+yarn
+
+npx parcel index.html
+```
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'Hello I am h1!!'),
+    React.createElement('h1', {}, 'Hello I am h1 from child1!!'),
     React.createElement('h2', {}, 'Hello I am h2!!'),
   ]),
   React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, 'Hello I am h1!!'),
+    React.createElement('h1', {}, 'Hello I am h1 from child2!!'),
     React.createElement('h2', {}, 'Hello I am h2!!'),
   ]),
 ]);
